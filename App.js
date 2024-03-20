b6b8ba4e1f9ee445e1d5b12850f41ef4dcd21409
MikesSzinyei <szinyeimkes@gmail.com>
2024-03-20 11:47:10 +0100

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import HomeScreen from './components/HomeScreen';
+import SettingsScreen from './components/SettingsScreen';
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer style={styles.container}>
+      <Tab.Navigator>
+        <Tab.Screen name="Főoldal" 
+          component={HomeScreen} />
+        <Tab.Screen name="Beállítások" 
+          component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
